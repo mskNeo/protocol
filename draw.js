ce1a420ddd8429158d66453fe9914250b5f5e1e4
antskinn <71914320+antskinn@users.noreply.github.com>
2021-04-24 21:04:14 -0400

    Colors Update
    Added random colors, and also a crazy rainbow mode that can be cchanged with the bool var crazyRainbowOn

--- draw.js
+++ draw.js
@@ -3,6 +3,8 @@ const btn = document.querySelector("button");
 const ctx = canvas.getContext("2d");
 let coord = { x: 0, y: 0 };
 
+var crazyRainbowOn = false;
+
 canvas.addEventListener("mousedown", start);
 canvas.addEventListener("mouseup", stop);
 window.addEventListener("resize", resize);
@@ -18,6 +20,17 @@ function start(event) {
   console.log('hi');
   document.addEventListener("mousemove", draw);
   reposition(event);
+  
+  if( !crazyRainbowOn )
+  {
+	  var red = Math.random() * 255;
+	  var green = Math.random() * 255;
+	  var blue = Math.random() * 255;
+	  ctx.strokeStyle = 'rgb('+
+		  red+','+
+		  green+','+
+		  blue+')';
+  }
 }
 
 function reposition(event) {
@@ -33,13 +46,26 @@ function draw(event) {
   ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#ACD3ED";
+  
+  if( crazyRainbowOn )
+  {
+	  var red = Math.random() * 255;
+	  var green = Math.random() * 255;
+	  var blue = Math.random() * 255;
+	  ctx.strokeStyle = 'rgb('+
+		  red+','+
+		  green+','+
+		  blue+')';
+  }
   ctx.moveTo(coord.x, coord.y);
   reposition(event);
   ctx.lineTo(coord.x, coord.y);
   ctx.stroke();
 }
 
+<<<<<<< Updated upstream
 btn.addEventListener('click', () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
+=======
+>>>>>>> Stashed changes
