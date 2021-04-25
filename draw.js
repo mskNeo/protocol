1892075bda6f2c669a94635182e188303c984010
antskinn <71914320+antskinn@users.noreply.github.com>
2021-04-24 21:19:13 -0400

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#canvas");
+const btn = document.querySelector("button");
 const ctx = canvas.getContext("2d");
 let coord = { x: 0, y: 0 };
+
+var crazyRainbowOn = false;
 
 canvas.addEventListener("mousedown", start);
 canvas.addEventListener("mouseup", stop);
@@ -14,8 +17,21 @@ function resize() {
 resize();
 
 function start(event) {
+  console.log('hi');
   document.addEventListener("mousemove", draw);
   reposition(event);
+  
+  if( !crazyRainbowOn )
+  {
+	  var red = Math.random() * 255;
+	  var green = Math.random() * 255;
+	  var blue = Math.random() * 255;
+	  ctx.strokeStyle = 'rgb('+
+		  red+','+
+		  green+','+
+		  blue+')';
+  }
+  
 }
 
 function reposition(event) {
@@ -31,9 +47,27 @@ function draw(event) {
   ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#ACD3ED";
+  
+  if( crazyRainbowOn )
+  {
+	  var red = Math.random() * 255;
+	  var green = Math.random() * 255;
+	  var blue = Math.random() * 255;
+	  ctx.strokeStyle = 'rgb('+
+		  red+','+
+		  green+','+
+		  blue+')';
+  }
   ctx.moveTo(coord.x, coord.y);
   reposition(event);
   ctx.lineTo(coord.x, coord.y);
   ctx.stroke();
 }
+
+
+
+btn.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+
